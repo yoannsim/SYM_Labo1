9e7f590a14465fe1a4baa7da00098e2ba0011353
isaia.spinelli <isaia.spinelli@heig-vd.ch>
2019-09-27 11:53:51 +0200

    add string to res and permission

--- Template/app/src/main/java/ch/heigvd/sym/template/MainActivity.java
+++ Template/app/src/main/java/ch/heigvd/sym/template/MainActivity.java
@@ -4,31 +4,40 @@
  * Author   : Markus Jaton 2 juillet 2014
  * 			  Fabien Dutoit 23 juillet 2019
  *            IICT / HEIG-VD
- *                                       
+ *
  * mailto:fabien.dutoit@heig-vd.ch
- * 
+ *
  * This piece of code reads a [email_account / password ] combination.
  * It is used as a template project for the SYM module element given at HEIG-VD
  * Target audience : students IL, TS, IE [generally semester 1, third bachelor year]
- *   
+ *
  * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
- * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
- * THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
- * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  */
 package ch.heigvd.sym.template;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
@@ -36,28 +45,29 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    // For logging purposes
-    private static final String TAG = MainActivity.class.getSimpleName();
+	// For logging purposes
+	private static final String TAG = MainActivity.class.getSimpleName();
 
-    // Just for test purposes : please destroy !
-	private static final String validEmail      = "toto@tutu.com";
-	private static final String validPassword   = "tata";
+	// Just for test purposes : please destroy !
+	private static final String validEmail = "toto@tutu.com";
+	private static final String validPassword = "tata";
 
-    // GUI elements
-	private EditText email       = null;
-	private EditText password      = null;
-    private Button   signIn     = null;
+	// GUI elements
+	private EditText email = null;
+	private EditText password = null;
+	private Button signIn = null;
 
+	@RequiresApi(api = Build.VERSION_CODES.M)
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
 		// Show the welcome screen / login authentication dialog
 		setContentView(R.layout.authent);
 
 		// Link to GUI elements
-        this.email      = findViewById(R.id.email);
-		this.password   = findViewById(R.id.psw);
-        this.signIn     = findViewById(R.id.buttOk);
+		this.email = findViewById(R.id.email);
+		this.password = findViewById(R.id.psw);
+		this.signIn = findViewById(R.id.buttOk);
 
 		// Then program action associated to "Ok" button
 		signIn.setOnClickListener((v) -> {
@@ -68,11 +78,9 @@ public class MainActivity extends AppCompatActivity {
 			 */
 			String mail = email.getText().toString();
 			String passwd = password.getText().toString(); //TODO read password from EditText
-			if (!mail.contains("@")){
-				// !!!!!!!!! "Il est nécessaire de mettre un email valide" ->  res/values/strings.xml)
-				Toast.makeText(MainActivity.this, "Il est nécessaire de mettre un email valide", Toast.LENGTH_SHORT).show();
-			}
-			else if (isValid(mail, passwd)) {
+			if (!mail.contains("@")) {
+				Toast.makeText(MainActivity.this, R.string.wrongEmail, Toast.LENGTH_SHORT).show();
+			} else if (isValid(mail, passwd)) {
 				/* Ok, valid combination, do something or launch another activity...
 				 * The current activity could be finished, but it is not mandatory.
 				 * To launch activity MyActivity.class, try something like :
@@ -91,17 +99,44 @@ public class MainActivity extends AppCompatActivity {
 				Intent intent = new Intent(this, ch.heigvd.sym.template.MyActivity.class);
 				intent.putExtra("emailEntered", mail);
 				intent.putExtra("passwordGiven", passwd);
-				this.startActivity(intent);
+				//this.startActivity(intent);
 
-				Toast.makeText(MainActivity.this, getResources().getString(R.string.good), Toast.LENGTH_LONG).show();
-				finish();
+				TelephonyManager telephonyManager;
+				telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+
+
+				if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+					// Permission is not granted
+					// Should we show an explanation?
+					if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+							Manifest.permission.READ_PHONE_STATE)) {
+						// Show an explanation to the user *asynchronously* -- don't block
+						// this thread waiting for the user's response! After the user
+						// sees the explanation, try again to request the permission.
+						Toast.makeText(MainActivity.this, "OK" , Toast.LENGTH_LONG).show();
+
+						return;
+					} else {
+						// No explanation needed; request the permission
+						ActivityCompat.requestPermissions(this,
+								new String[]{Manifest.permission.READ_PHONE_STATE},2);
+
+						// MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+						// app-defined int constant. The callback method gets the
+						// result of the request.
+						Toast.makeText(MainActivity.this, "Pas ok", Toast.LENGTH_LONG).show();
+
+						return;
+					}
+				}
+				String IMEI_Number_Holder = telephonyManager.getDeviceId();
+
+				Toast.makeText(MainActivity.this, getResources().getString(R.string.good) + " " + IMEI_Number_Holder, Toast.LENGTH_LONG).show();
+				//finish();
 			} else {
 				// Wrong combination, display pop-up dialog and stay on login screen
 				email.setText("");
 				password.setText("");
-				/*
-				!!!!!!!!!!!!! Utiliser un DialogBuilder ! ("Utilisateur ou mot de passe inconnu" ->  res/values/strings.xml)
-				*/
 				showErrorDialog(mail, passwd);
 			}
 		});
